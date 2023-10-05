b041ad560f01dd520445c24ea49d55a8f2c7a262
jaiel666 <mgusts23@gmail.com>
2023-10-05 19:40:28 +0300

    uzdevumi

--- loops/08.php
+++ loops/08.php
@@ -1,13 +1,29 @@
 <?php
 class NumberSquare {
-    public static function minmax() {
+    public static function minmax()
+    {
         echo "Min? ";
-        $min = readline();
+        $min = intval(readline());
 
         echo "Max? ";
-        $max = readline();
+        $max = intval(readline());
 
         if ($min > $max) {
             echo "Min should be less than or equal to Max.\n";
-            return;
+        } else {
+            for ($i = 0; $i <= $max - $min; $i++) {
+                for ($x = $min; $x <= $max; $x++) {
+                    $value = $x + $i;
+                    if ($value > $max) {
+                        echo $value - ($max - $min) - 1;
+                    } else {
+                        echo $value;
+                    }
+                }
+                echo "\n";
+            }
         }
+    }
+}
+
+NumberSquare::minmax();
