ea6124c0838cc82ca14cd070b72da3fb6dc70336
jaiel666 <mgusts23@gmail.com>
2023-10-06 13:07:36 +0300

    uzdevumi

--- classes-objects/04.php
+++ classes-objects/04.php
@@ -1,1 +1,45 @@
 <?php
+
+class Movie
+{
+    public $title;
+    public $studio;
+    public $rating;
+
+    public function __construct($title, $studio, $rating)
+    {
+        $this->title = $title;
+        $this->studio = $studio;
+        $this->rating = $rating;
+    }
+}
+
+function GetPG($movies) {
+    $pgMovies = array();
+    $count = count($movies);
+
+    for ($i = 0; $i < $count; $i++) {
+        if ($movies[$i]->rating === "PG") {
+            $pgMovies[] = $movies[$i];
+        }
+    }
+
+    return $pgMovies;
+}
+
+$movie1 = new Movie("Casino Royale", "Eon Productions", "PG-13");
+$movie2 = new Movie("Glass", "Buena Vista International", "PG-13");
+$movie3 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+$movie4 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+$movie5 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+$movies = array($movie1, $movie2, $movie3, $movie4, $movie5);
+$pgRatedMovies = GetPG($movies);
+
+$count = count($pgRatedMovies);
+for ($i = 0; $i < $count; $i++) {
+    echo "Title: " . $pgRatedMovies[$i]->title . "\n";
+    echo "Studio: " . $pgRatedMovies[$i]->studio . "\n";
+    echo "Rating: " . $pgRatedMovies[$i]->rating . "\n";
+    echo "------------------------\n";
+}
--- classes-objects/05.php
+++ classes-objects/05.php
@@ -1,1 +1,60 @@
 <?php
+
+class Date
+{
+    private $month;
+    private $day;
+    private $year;
+
+    public function __construct($month, $day, $year)
+    {
+        $this->month = $month;
+        $this->day = $day;
+        $this->year = $year;
+    }
+
+    public function getMonth()
+    {
+        return $this->month;
+    }
+
+    public function setMonth($month)
+    {
+        $this->month = $month;
+    }
+
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    public function setDay($day)
+    {
+        $this->day = $day;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    public function displayDate()
+    {
+        return $this->month . '/' . $this->day . '/' . $this->year;
+    }
+}
+
+$date = new Date(10, 5, 2023);
+
+echo "Initial Date: " . $date->displayDate() . "\n";
+
+$date->setMonth(10);
+$date->setDay(6);
+$date->setYear(2023);
+
+echo "Modified Date: " . $date->displayDate() . "\n";
